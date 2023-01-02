275aba94a269ffa7573027f29d78908f9709e3f1
ckwatt11 <ckwatt11@gmail.com>
2023-01-02 00:04:01 -0500

    add ctrl-c support to kill server

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -3,10 +3,10 @@ const webSocket = require("ws");
 const http = require("http");
 const {v4: uuidv4} = require("uuid");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 6060;
 const server = http.createServer(app); // 
 
-const webSocketServer = new WebSocket.Server({server}); // initialize the web socket server to add onto app.
+const webSocketServer = new webSocket.Server({server}); // initialize the web socket server to add onto app.
 
 webSocketServer.on("connection:", ws => {
     ws.on("message", msg => {
@@ -16,11 +16,23 @@ webSocketServer.on("connection:", ws => {
     ws.send(JSON.stringify({
         type: "connect", 
         message: "WebSocket server initialized."
-    })
+        })
     );
 
 });
 
-server.listen(port, () => {
+server.listen(PORT, () => {  // event listener to handle incoming connections from clients. 
     console.log(`The server is running on port ${PORT}`);
 });
+
+let userList = {}; // keeps track of all connected users
+
+app.get('/', function(req, res, next) {
+    res.send("Hello world");
+});
+
+process.on('SIGINT', function() {
+    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+    // some other closing procedures go here
+    process.exit(0);
+  });
