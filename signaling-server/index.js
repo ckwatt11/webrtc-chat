a8b1495d53485a86afff7e814e65dc2d15b7e74d
ckwatt11 <ckwatt11@gmail.com>
2023-01-02 21:13:11 -0500

    update index.js

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -1,38 +1,108 @@
 const express = require("express");
-const webSocket = require("ws");
+const WebSocket = require("ws");
 const http = require("http");
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 const app = express();
-const PORT = process.env.PORT || 6060;
-const server = http.createServer(app); // 
 
-const webSocketServer = new webSocket.Server({server}); // initialize the web socket server to add onto app.
+const serverPort = process.env.PORT || 9000;
 
-webSocketServer.on("connection:", ws => {
-    ws.on("message", msg => {
-        console.log(`Message received from client: ${msg}`);
-    });
 
-    ws.send(JSON.stringify({
-        type: "connect", 
-        message: "WebSocket server initialized."
-        })
-    );
+const server = http.createServer(app);
 
+const webSocketServer = new WebSocket.Server({ server });
+
+webSocketServer.on("connection", ws => {
+  ws.on("message", msg => {
+    console.log("Recieved from client : %s", msg);
+    let content; // only accepts content in a valid JSON format
+
+    try {
+      content = JSON.parse(msg);
+    } catch (err) {
+      console.log("Invalid JSON");
+      content = {};
+    }
+    const {type, name, offer, ans, candidate} = content; 
+    switch (type){
+      case "login":
+        if (users[name]) {
+          sendMsg(ws, {
+            type: "login",
+            successful: false, 
+            message: "Username already taken. Please choose another. "
+          });
+        } // username already exists
+        else{
+          const uid = uuidv4();
+          const isLoggedIn = Object.values(
+            users   
+          ).map(({uid, name : username}) => ({uid, username}));
+          users[name] = ws; 
+          ws.name = name; 
+          ws.id = uid; 
+
+          sendMsg(ws, {
+            type: "login",
+            successful: true,
+            users: isLoggedIn
+          });
+          broadcastMsg(users, "updateUsers", ws);
+        }
+        break;
+      case "offer":
+        const recipient = users[name];
+        if (!!recipient){
+          sendMsg(recipient, {
+            type: "offer", 
+            offer, 
+            name: ws.name
+          });
+        } else {
+          
+        }
+    }   
+
+  });
+  
+  ws.send(   // feedback msg upon successful connection
+    JSON.stringify({
+      type: "connect",
+      message: "Web Socket Server has been initialized"
+    })
+  );
 });
 
-server.listen(PORT, () => {  // event listener to handle incoming connections from clients. 
-    console.log(`The server is running on port ${PORT}`);
-});
 
-let userList = {}; // keeps track of all connected users
-
-app.get('/', function(req, res, next) {
-    res.send("Hello world");
+server.listen(serverPort, () => {
+  console.log(`Signaling Server running on port: ${serverPort}`);
 });
 
 process.on('SIGINT', function() {
     console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
     // some other closing procedures go here
     process.exit(0);
+});
+
+app.get('/', function(req, res, next) {
+    res.send("Hello world");
+});
+
+// create functions for basic chat operations
+
+const sendMsg = (connection, msg) => {
+  connection.send(JSON.stringify(msg));
+};
+
+const broadcastMsg = (clients, type, { id, name: username }) => {
+  Object.values(clients).forEach(user => {
+    if (user.name !== username) {
+      user.send(
+        JSON.stringify({
+          type,
+          user: { id, username }
+        })
+      );
+    }
+    
   });
+};
